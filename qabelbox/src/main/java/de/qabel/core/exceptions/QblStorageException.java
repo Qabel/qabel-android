184f2a9343ef65144c7a71d78f5af50e267f2398
Jens Kadenbach <gitdax@daxbau.net>
2015-11-24 10:20:49 +0100

    Create QblStorage* exceptions
    The qabel-core library is compiled with the PR
    https://github.com/Qabel/qabel-core/pull/403
    already included.
    
    Resolves #15

---
+++ qabelbox/src/main/java/de/qabel/core/exceptions/QblStorageException.java
@@ -0,0 +1,11 @@
+package de.qabel.core.exceptions;
+
+public class QblStorageException extends QblException{
+	public QblStorageException(Throwable e) {
+		super(e);
+	}
+
+	public QblStorageException(String s) {
+		super(s);
+	}
+}

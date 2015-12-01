68f946a939de61c13ae1619083ce26e95955740d
L-Henke <henke@praemandatum.de>
2015-12-01 12:39:20 +0100

    Merged QabelLauncher into QabelBox
    There probably are some rough edges, but it mainly seems to work.

---
+++ libqabel/src/main/java/de/qabel/QabelContentProviderConstants.java
@@ -0,0 +1,11 @@
+package de.qabel;
+
+public interface QabelContentProviderConstants {
+
+    String CONTENT_AUTHORITY = "de.qabel.qabelbox.services.QabelContentProvider";
+    String CONTENT_CONTACTS = "contacts";
+    String CONTENT_IDENTITIES = "identities";
+
+    String[] CONTACT_COLUMN_NAMES = new String[]{"name", "owner_id", "id"};
+    String[] IDENTITIES_COLUMN_NAMES = new String[]{"name", "id"};
+}
---
+++ libqabel/src/main/java/de/qabel/ServiceConstants.java
@@ -0,0 +1,22 @@
+package de.qabel;
+
+
+/**
+ * Interface for service related constants
+ */
+public interface ServiceConstants {
+
+    String SERVICE_PACKAGE_NAME = "de.qabel.qabelbox";
+    String SERVICE_CLASS_NAME = "de.qabel.qabelbox.service.QabelService";
+
+    int MSG_REGISTER_ON_TYPE = 0;
+    int MSG_DROP_MESSAGE = 1;
+
+    String DROP_MESSAGE_TYPE = "DropMessageType";
+    String DROP_MESSAGE = "DropMessage";
+
+    String DROP_PAYLOAD_TYPE = "DropPayloadType";
+    String DROP_PAYLOAD = "DropPayload";
+    String DROP_RECIPIENT_ID = "DropRecipientId";
+    String DROP_SENDER_ID = "DropSenderID";
+}

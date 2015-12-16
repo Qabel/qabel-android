a132932a44af165f4a3ef0a28dbb215536e312e1
David Hansmann <hansmann.d@debitel.net>
2015-12-16 16:39:08 +0100

    Search by name #26: Convert intendation to tabs

--- qabelbox/src/androidTest/java/de/qabel/qabelbox/storage/SearchTest.java
+++ qabelbox/src/androidTest/java/de/qabel/qabelbox/storage/SearchTest.java
@@ -35,267 +35,266 @@ import static org.junit.Assert.assertThat;
  */
 public class SearchTest extends AndroidTestCase {
 
-    private final static String TAG = SearchTest.class.getName();
-
-    //will represent a filled resultset after setUp()
-    //
-    //level0-one.bin
-    //+dir1-level1-one
-    //  level1-ONE.bin
-    //  level1-two-Small.bin
-    //  +dir1-level2-one
-    //      one-level2-one.bin
-    //  +dir1-level2-two
-    //      two-level2-one.bin
-    //
-    private static List<BoxObject> searchResults;
-
-    private static boolean setup = true;
-
-    public void setUp() throws Exception {
-        if (!setup) {
-            //setting up the directory structure takes time and so it is only made once - @AfterClass is not available here, so the cleanup is done, too
-            return;
-        }
-
-        setup = false;
-
-        AmazonS3Client s3Client = null;
-        final String bucket = "qabel";
-        final String prefix = UUID.randomUUID().toString();
-
-        try {
-            CryptoUtils utils = new CryptoUtils();
-            byte[] deviceID = utils.getRandomBytes(16);
-            QblECKeyPair keyPair = new QblECKeyPair();
+	private final static String TAG = SearchTest.class.getName();
+
+	//will represent a filled resultset after setUp()
+	//
+	//level0-one.bin
+	//+dir1-level1-one
+	//  level1-ONE.bin
+	//  level1-two-Small.bin
+	//  +dir1-level2-one
+	//      one-level2-one.bin
+	//  +dir1-level2-two
+	//      two-level2-one.bin
+	//
+	private static List<BoxObject> searchResults;
+
+	private static boolean setup = true;
+
+	public void setUp() throws Exception {
+		if (!setup) {
+			//setting up the directory structure takes time and so it is only made once - @AfterClass is not available here, so the cleanup is done, too
+			return;
+		}
+
+		setup = false;
+
+		AmazonS3Client s3Client = null;
+		final String bucket = "qabel";
+		final String prefix = UUID.randomUUID().toString();
+
+		try {
+			CryptoUtils utils = new CryptoUtils();
+			byte[] deviceID = utils.getRandomBytes(16);
+			QblECKeyPair keyPair = new QblECKeyPair();
 
-            AWSCredentials awsCredentials = new AWSCredentials() {
-                @Override
-                public String getAWSAccessKeyId() {
-                    return getContext().getResources().getString(R.string.aws_user);
-                }
+			AWSCredentials awsCredentials = new AWSCredentials() {
+				@Override
+				public String getAWSAccessKeyId() {
+					return getContext().getResources().getString(R.string.aws_user);
+				}
 
-                @Override
-                public String getAWSSecretKey() {
-                    return getContext().getString(R.string.aws_password);
-                }
-            };
-            AWSCredentials credentials = awsCredentials;
-            s3Client = new AmazonS3Client(credentials);
-            assertNotNull(awsCredentials.getAWSAccessKeyId());
-            assertNotNull(awsCredentials.getAWSSecretKey());
+				@Override
+				public String getAWSSecretKey() {
+					return getContext().getString(R.string.aws_password);
+				}
+			};
+			AWSCredentials credentials = awsCredentials;
+			s3Client = new AmazonS3Client(credentials);
+			assertNotNull(awsCredentials.getAWSAccessKeyId());
+			assertNotNull(awsCredentials.getAWSSecretKey());
 
-            TransferUtility transfer = new TransferUtility(s3Client, getContext());
-            BoxVolume volume = new BoxVolume(transfer, credentials, keyPair, bucket, prefix,
-                    deviceID, getContext());
+			TransferUtility transfer = new TransferUtility(s3Client, getContext());
+			BoxVolume volume = new BoxVolume(transfer, credentials, keyPair, bucket, prefix,
+					deviceID, getContext());
 
-            volume.createIndex(bucket, prefix);
+			volume.createIndex(bucket, prefix);
 
-            Log.d(TAG, "VOL :" + volume.toString());
-
-            BoxNavigation nav = volume.navigate();
+			Log.d(TAG, "VOL :" + volume.toString());
+
+			BoxNavigation nav = volume.navigate();
 
-            setupFakeDirectoryStructure(nav);
-
-            setupBaseSearch(nav);
-        } finally {
-            if (s3Client != null) {
-                cleanUp(s3Client, bucket, prefix);
-            }
-        }
+			setupFakeDirectoryStructure(nav);
+
+			setupBaseSearch(nav);
+		} finally {
+			if (s3Client != null) {
+				cleanUp(s3Client, bucket, prefix);
+			}
+		}
 
-        Log.d(TAG, "SETUP DONE");
-    }
+		Log.d(TAG, "SETUP DONE");
+	}
 
-    public void cleanUp(AmazonS3Client s3Client, String bucket, String prefix) throws IOException {
-        ObjectListing listing = s3Client.listObjects(bucket, prefix);
-        List<DeleteObjectsRequest.KeyVersion> keys = new ArrayList<>();
+	public void cleanUp(AmazonS3Client s3Client, String bucket, String prefix) throws IOException {
+		ObjectListing listing = s3Client.listObjects(bucket, prefix);
+		List<DeleteObjectsRequest.KeyVersion> keys = new ArrayList<>();
 
-        for (S3ObjectSummary summary : listing.getObjectSummaries()) {
-            Log.d(TAG, "DELETE: " + summary.getKey());
-            keys.add(new DeleteObjectsRequest.KeyVersion(summary.getKey()));
-        }
+		for (S3ObjectSummary summary : listing.getObjectSummaries()) {
+			Log.d(TAG, "DELETE: " + summary.getKey());
+			keys.add(new DeleteObjectsRequest.KeyVersion(summary.getKey()));
+		}
 
-        if (keys.isEmpty()) {
-            return;
-        }
+		if (keys.isEmpty()) {
+			return;
+		}
 
-        DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(bucket);
-        deleteObjectsRequest.setKeys(keys);
-        s3Client.deleteObjects(deleteObjectsRequest);
-    }
+		DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(bucket);
+		deleteObjectsRequest.setKeys(keys);
+		s3Client.deleteObjects(deleteObjectsRequest);
+	}
 
-    private void setupFakeDirectoryStructure(BoxNavigation nav) throws Exception {
+	private void setupFakeDirectoryStructure(BoxNavigation nav) throws Exception {
 
-        String testFile = BoxTest.createTestFile();
-        String smallFile = BoxTest.smallTestFile().getAbsolutePath();
+		String testFile = BoxTest.createTestFile();
+		String smallFile = BoxTest.smallTestFile().getAbsolutePath();
 
-        assertThat(nav.listFiles().size(), is(0));
+		assertThat(nav.listFiles().size(), is(0));
 
-        nav.upload("level0-one.bin", new FileInputStream(testFile), null);
-        nav.commit();
+		nav.upload("level0-one.bin", new FileInputStream(testFile), null);
+		nav.commit();
 
-        BoxFolder folder = nav.createFolder("dir1-level1-one");
-        nav.commit();
-        nav.navigate(folder);
+		BoxFolder folder = nav.createFolder("dir1-level1-one");
+		nav.commit();
+		nav.navigate(folder);
 
-        nav.upload("level1-ONE.bin", new FileInputStream(testFile), null);
-        nav.commit();
-        nav.upload("level1-two-Small.bin", new FileInputStream(smallFile), null);
-        nav.commit();
+		nav.upload("level1-ONE.bin", new FileInputStream(testFile), null);
+		nav.commit();
+		nav.upload("level1-two-Small.bin", new FileInputStream(smallFile), null);
+		nav.commit();
 
-        folder = nav.createFolder("dir1-level2-one");
-        nav.commit();
-        nav.navigate(folder);
+		folder = nav.createFolder("dir1-level2-one");
+		nav.commit();
+		nav.navigate(folder);
 
-        nav.upload("one-level2-one.bin", new FileInputStream(testFile), null);
-        nav.commit();
+		nav.upload("one-level2-one.bin", new FileInputStream(testFile), null);
+		nav.commit();
 
-        nav.navigateToParent();
+		nav.navigateToParent();
 
-        folder = nav.createFolder("dir1-level2-two");
-        nav.commit();
-        nav.navigate(folder);
+		folder = nav.createFolder("dir1-level2-two");
+		nav.commit();
+		nav.navigate(folder);
 
-        nav.upload("two-level2-one.bin", new FileInputStream(testFile), null);
-        nav.commit();
+		nav.upload("two-level2-one.bin", new FileInputStream(testFile), null);
+		nav.commit();
 
-        while (nav.hasParent()) {
-            nav.navigateToParent();
-        }
+		while (nav.hasParent()) {
+			nav.navigateToParent();
+		}
 
-        Log.d(TAG, "NAV : " + nav);
+		Log.d(TAG, "NAV : " + nav);
 
-        debug(nav);
+		debug(nav);
 
-    }
+	}
 
-    private void debug(BoxNavigation nav) throws Exception {
+	private void debug(BoxNavigation nav) throws Exception {
 
-        for (BoxFile file : nav.listFiles()) {
-            Log.d(TAG, "FILE: " + file.name);
-        }
+		for (BoxFile file : nav.listFiles()) {
+			Log.d(TAG, "FILE: " + file.name);
+		}
 
-        for (BoxFolder folder : nav.listFolders()) {
-            Log.d(TAG, "DIR : " + folder.name);
+		for (BoxFolder folder : nav.listFolders()) {
+			Log.d(TAG, "DIR : " + folder.name);
 
-            nav.navigate(folder);
-            debug(nav);
-            nav.navigateToParent();
-        }
-    }
+			nav.navigate(folder);
+			debug(nav);
+			nav.navigateToParent();
+		}
+	}
 
-    private void debug(BoxObject o) {
-        if (o instanceof BoxFile) {
-            Log.d(TAG, "FILE: " + o.name + " @" + ((BoxFile) o).size);
-        }
-        else {
-            Log.d(TAG, "DIR : " + o.name);
-        }
-    }
+	private void debug(BoxObject o) {
+		if (o instanceof BoxFile) {
+			Log.d(TAG, "FILE: " + o.name + " @" + ((BoxFile) o).size);
+		} else {
+			Log.d(TAG, "DIR : " + o.name);
+		}
+	}
 
-    private void setupBaseSearch(BoxNavigation nav) throws Exception {
-        searchResults = new StorageSearch(nav).getResults();
-    }
+	private void setupBaseSearch(BoxNavigation nav) throws Exception {
+		searchResults = new StorageSearch(nav).getResults();
+	}
 
-    @Test
-    public void testCollectAll() throws Exception {
+	@Test
+	public void testCollectAll() throws Exception {
 
-        Log.d(TAG, "collectAll");
+		Log.d(TAG, "collectAll");
 
-        for (BoxObject o : searchResults) {
-            debug(o);
-        }
+		for (BoxObject o : searchResults) {
+			debug(o);
+		}
 
-        assertEquals(8, searchResults.size());
+		assertEquals(8, searchResults.size());
 
-        Log.d(TAG, "/collectAll");
-    }
+		Log.d(TAG, "/collectAll");
+	}
 
-    @Test
-    public void testForValidName() throws Exception {
-        assertFalse(StorageSearch.isValidSearchTerm(null));
-        assertFalse(StorageSearch.isValidSearchTerm(""));
-        assertFalse(StorageSearch.isValidSearchTerm(" "));
+	@Test
+	public void testForValidName() throws Exception {
+		assertFalse(StorageSearch.isValidSearchTerm(null));
+		assertFalse(StorageSearch.isValidSearchTerm(""));
+		assertFalse(StorageSearch.isValidSearchTerm(" "));
 
-        assertTrue(StorageSearch.isValidSearchTerm("1"));
-    }
+		assertTrue(StorageSearch.isValidSearchTerm("1"));
+	}
 
-    @Test
-    public void testNameSearch() throws Exception {
-        List<BoxObject> lst = new StorageSearch(searchResults).filterByName("small").getResults();
+	@Test
+	public void testNameSearch() throws Exception {
+		List<BoxObject> lst = new StorageSearch(searchResults).filterByName("small").getResults();
 
-        assertEquals(1, lst.size());
-        assertEquals("level1-two-Small.bin", lst.get(0).name);
+		assertEquals(1, lst.size());
+		assertEquals("level1-two-Small.bin", lst.get(0).name);
 
-        StorageSearch search = new StorageSearch(searchResults).filterByNameCaseSensitive("small");
-        assertEquals(0, search.getResults().size());
+		StorageSearch search = new StorageSearch(searchResults).filterByNameCaseSensitive("small");
+		assertEquals(0, search.getResults().size());
 
-        search =  new StorageSearch(searchResults).filterByNameCaseSensitive("Small");
-        assertEquals(1, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("Small");
+		assertEquals(1, search.getResults().size());
 
-        //if not valid don't apply the filter
-        search =  new StorageSearch(searchResults).filterByName(null);
-        assertEquals(8, search.getResults().size());
+		//if not valid don't apply the filter
+		search = new StorageSearch(searchResults).filterByName(null);
+		assertEquals(8, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByName("");
-        assertEquals(8, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByName("");
+		assertEquals(8, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByName(" ");
-        assertEquals(8, search.getResults().size());
-    }
+		search = new StorageSearch(searchResults).filterByName(" ");
+		assertEquals(8, search.getResults().size());
+	}
 
-    @Test
-    public void testFilterBySize() throws Exception {
+	@Test
+	public void testFilterBySize() throws Exception {
 
-        StorageSearch search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1");
-        assertEquals(3, search.getResults().size());
+		StorageSearch search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1");
+		assertEquals(3, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterByMaximumSize(100);
-        assertEquals(1, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterByMaximumSize(100);
+		assertEquals(1, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterByMaximumSize(110000);
-        assertEquals(2, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterByMaximumSize(110000);
+		assertEquals(2, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterByMaximumSize(100000);
-        assertEquals(1, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterByMaximumSize(100000);
+		assertEquals(1, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterByMinimumSize(1);
-        assertEquals(2, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterByMinimumSize(1);
+		assertEquals(2, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterByMinimumSize(100);
-        assertEquals(1, search.getResults().size());
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterByMinimumSize(100);
+		assertEquals(1, search.getResults().size());
 
-        search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterByMinimumSize(10000000);
-        assertEquals(0, search.getResults().size());
-    }
+		search = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterByMinimumSize(10000000);
+		assertEquals(0, search.getResults().size());
+	}
 
-    @Test
-    public void testFilterByFileOrDir() throws Exception {
+	@Test
+	public void testFilterByFileOrDir() throws Exception {
 
-        List<BoxObject> objs = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterOnlyDirectories().getResults();
-        assertEquals(1, objs.size());
+		List<BoxObject> objs = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterOnlyDirectories().getResults();
+		assertEquals(1, objs.size());
 
-        List<BoxFolder> dirs = StorageSearch.toBoxFolders(objs);
-        assertEquals(1, dirs.size());
-        assertEquals("dir1-level1-one", dirs.get(0).name);
+		List<BoxFolder> dirs = StorageSearch.toBoxFolders(objs);
+		assertEquals(1, dirs.size());
+		assertEquals("dir1-level1-one", dirs.get(0).name);
 
-        objs = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
-                .filterOnlyFiles().getResults();
-        assertEquals(2, objs.size());
+		objs = new StorageSearch(searchResults).filterByNameCaseSensitive("level1")
+				.filterOnlyFiles().getResults();
+		assertEquals(2, objs.size());
 
-        List<BoxFile> files = StorageSearch.toBoxFiles(objs);
-        assertEquals(2, files.size());
-        assertEquals("level1-ONE.bin", files.get(0).name);
-        assertEquals("level1-two-Small.bin", files.get(1).name);
-    }
+		List<BoxFile> files = StorageSearch.toBoxFiles(objs);
+		assertEquals(2, files.size());
+		assertEquals("level1-ONE.bin", files.get(0).name);
+		assertEquals("level1-two-Small.bin", files.get(1).name);
+	}
 
 
 }
--- qabelbox/src/main/java/de/qabel/qabelbox/storage/StorageSearch.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/storage/StorageSearch.java
@@ -10,170 +10,170 @@ import de.qabel.qabelbox.exceptions.QblStorageException;
  */
 public class StorageSearch {
 
-    private BoxNavigation navigation;
-    private List<BoxObject> results;
+	private BoxNavigation navigation;
+	private List<BoxObject> results;
 
-    /**
-     * Inits the results with all available files and directories.
-     *
-     * @param navigation The path from which on the search should begin
-     * @throws QblStorageException
-     */
-    public StorageSearch(BoxNavigation navigation) throws QblStorageException {
-        this.navigation = navigation;
-        results = collectAll();
-    }
+	/**
+	 * Inits the results with all available files and directories.
+	 *
+	 * @param navigation The path from which on the search should begin
+	 * @throws QblStorageException
+	 */
+	public StorageSearch(BoxNavigation navigation) throws QblStorageException {
+		this.navigation = navigation;
+		results = collectAll();
+	}
 
-    /**
-     * Construct a search using a resultset and don't hit the storage volume.
-     *
-     * @param results
-     */
-    public StorageSearch(List<BoxObject> results) {
-        this.results = results;
-    }
+	/**
+	 * Construct a search using a resultset and don't hit the storage volume.
+	 *
+	 * @param results
+	 */
+	public StorageSearch(List<BoxObject> results) {
+		this.results = results;
+	}
 
-    /**
-     * The flat list of the current resultset.
-     *
-     * @return The filtered list, all files and dirs if no filter was applied.
-     */
-    public List<BoxObject> getResults() {
-        return results;
-    }
+	/**
+	 * The flat list of the current resultset.
+	 *
+	 * @return The filtered list, all files and dirs if no filter was applied.
+	 */
+	public List<BoxObject> getResults() {
+		return results;
+	}
 
-    public static boolean isValidSearchTerm(String name) {
-        return name != null && !"".equals(name.trim());
-    }
+	public static boolean isValidSearchTerm(String name) {
+		return name != null && !"".equals(name.trim());
+	}
 
-    public StorageSearch filterByNameCaseSensitive(String name) {
-        return filterByName(name, true);
-    }
+	public StorageSearch filterByNameCaseSensitive(String name) {
+		return filterByName(name, true);
+	}
 
-    public StorageSearch filterByNameCaseInsensitive(String name) {
-        return filterByName(name, false);
-    }
+	public StorageSearch filterByNameCaseInsensitive(String name) {
+		return filterByName(name, false);
+	}
 
-    public StorageSearch filterByName(String name) {
-        return filterByNameCaseInsensitive(name);
-    }
+	public StorageSearch filterByName(String name) {
+		return filterByNameCaseInsensitive(name);
+	}
 
-    public StorageSearch filterByName(String name, boolean caseSensitive) {
+	public StorageSearch filterByName(String name, boolean caseSensitive) {
 
-        if (!isValidSearchTerm(name)) {
-            return this;
-        }
+		if (!isValidSearchTerm(name)) {
+			return this;
+		}
 
-        if (!caseSensitive) {
-            name = name.toLowerCase();
-        }
+		if (!caseSensitive) {
+			name = name.toLowerCase();
+		}
 
-        List<BoxObject> filtered = new ArrayList<>();
+		List<BoxObject> filtered = new ArrayList<>();
 
-        for (BoxObject o : results) {
-            String objKey = caseSensitive ? o.name : o.name.toLowerCase();
+		for (BoxObject o : results) {
+			String objKey = caseSensitive ? o.name : o.name.toLowerCase();
 
-            if (objKey.indexOf(name) >= 0) {
-                filtered.add(o);
-            }
-        }
+			if (objKey.indexOf(name) >= 0) {
+				filtered.add(o);
+			}
+		}
 
-        results = filtered;
+		results = filtered;
 
-        return this;
-    }
+		return this;
+	}
 
-    public StorageSearch filterByMaximumSize(long size) {
-        return filterBySize(size, false);
-    }
+	public StorageSearch filterByMaximumSize(long size) {
+		return filterBySize(size, false);
+	}
 
-    public StorageSearch filterByMinimumSize(long size) {
-        return filterBySize(size, true);
-    }
+	public StorageSearch filterByMinimumSize(long size) {
+		return filterBySize(size, true);
+	}
 
-    public StorageSearch filterBySize(long size, boolean minSize) {
+	public StorageSearch filterBySize(long size, boolean minSize) {
 
-        List<BoxObject> filtered = new ArrayList<>();
+		List<BoxObject> filtered = new ArrayList<>();
 
-        for (BoxObject o : results) {
-            if (o instanceof BoxFile) {
-                BoxFile f = (BoxFile) o;
+		for (BoxObject o : results) {
+			if (o instanceof BoxFile) {
+				BoxFile f = (BoxFile) o;
 
-                if ((minSize && f.size >= size) || (!minSize && f.size <= size)) {
-                    filtered.add(o);
-                }
-            }
-        }
+				if ((minSize && f.size >= size) || (!minSize && f.size <= size)) {
+					filtered.add(o);
+				}
+			}
+		}
 
-        results = filtered;
+		results = filtered;
 
-        return this;
-    }
+		return this;
+	}
 
-    public StorageSearch filterOnlyFiles() {
+	public StorageSearch filterOnlyFiles() {
 
-        List<BoxObject> filtered = new ArrayList<>();
-        filtered.addAll(StorageSearch.toBoxFiles(results));
-        results = filtered;
+		List<BoxObject> filtered = new ArrayList<>();
+		filtered.addAll(StorageSearch.toBoxFiles(results));
+		results = filtered;
 
-        return this;
-    }
+		return this;
+	}
 
-    public static List<BoxFile> toBoxFiles(List<BoxObject> lst) {
-        List<BoxFile> ret = new ArrayList<>();
+	public static List<BoxFile> toBoxFiles(List<BoxObject> lst) {
+		List<BoxFile> ret = new ArrayList<>();
 
-        for (BoxObject o : lst) {
-            if (o instanceof BoxFile) {
-                ret.add((BoxFile) o);
-            }
-        }
+		for (BoxObject o : lst) {
+			if (o instanceof BoxFile) {
+				ret.add((BoxFile) o);
+			}
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 
-    public StorageSearch filterOnlyDirectories() {
+	public StorageSearch filterOnlyDirectories() {
 
-        List<BoxObject> filtered = new ArrayList<>();
-        filtered.addAll(StorageSearch.toBoxFolders(results));
-        results = filtered;
+		List<BoxObject> filtered = new ArrayList<>();
+		filtered.addAll(StorageSearch.toBoxFolders(results));
+		results = filtered;
 
-        return this;
-    }
+		return this;
+	}
 
-    public static List<BoxFolder> toBoxFolders(List<BoxObject> lst) {
-        List<BoxFolder> ret = new ArrayList<>();
+	public static List<BoxFolder> toBoxFolders(List<BoxObject> lst) {
+		List<BoxFolder> ret = new ArrayList<>();
 
-        for (BoxObject o : lst) {
-            if (o instanceof BoxFolder) {
-                ret.add((BoxFolder) o);
-            }
-        }
+		for (BoxObject o : lst) {
+			if (o instanceof BoxFolder) {
+				ret.add((BoxFolder) o);
+			}
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 
 
-    private List<BoxObject> collectAll() throws QblStorageException {
-        List<BoxObject> lst = new ArrayList<>();
+	private List<BoxObject> collectAll() throws QblStorageException {
+		List<BoxObject> lst = new ArrayList<>();
 
-        addAll(lst);
+		addAll(lst);
 
-        return lst;
-    }
+		return lst;
+	}
 
-    private void addAll(List<BoxObject> lst) throws QblStorageException {
+	private void addAll(List<BoxObject> lst) throws QblStorageException {
 
-        for (BoxFile file : navigation.listFiles()) {
-            lst.add(file);
-        }
+		for (BoxFile file : navigation.listFiles()) {
+			lst.add(file);
+		}
 
-        for (BoxFolder folder : navigation.listFolders()) {
-            lst.add(folder);
+		for (BoxFolder folder : navigation.listFolders()) {
+			lst.add(folder);
 
-            navigation.navigate(folder);
-            addAll(lst);
-            navigation.navigateToParent();
-        }
-    }
+			navigation.navigate(folder);
+			addAll(lst);
+			navigation.navigateToParent();
+		}
+	}
 
 }

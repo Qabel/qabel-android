0c8fb3fd8b56b3e3fc800449bd9ce98b885149b5
Danny Werner <danny.werner@iconmobile.com>
2016-01-12 15:57:31 +0100

    add fontview classes

---
+++ qabelbox/src/main/java/de/qabel/qabelbox/views/CheckBoxFont.java
@@ -0,0 +1,35 @@
+package de.qabel.qabelbox.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+
+import de.qabel.qabelbox.helper.FontHelper;
+
+/**
+ * Created by danny on 12.01.2016.
+ */
+public class CheckBoxFont extends CheckBox {
+    public CheckBoxFont(Context context) {
+        super(context);
+        FontHelper.getInstance(context).setCustomeFonts(this);
+    }
+
+    public CheckBoxFont(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        FontHelper.getInstance(context).setCustomeFonts(this);
+    }
+
+    public CheckBoxFont(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        FontHelper.getInstance(context).setCustomeFonts(this);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CheckBoxFont(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        FontHelper.getInstance(context).setCustomeFonts(this);
+    }
+}

d60881ea68c1f7d1c4c4c25ec9060898f4c12e2e
Danny Werner <danny.werner@iconmobile.com>
2016-01-13 13:24:38 +0100

    add splashscreen

--- qabelbox/src/main/java/de/qabel/qabelbox/activities/SplashActivity.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/activities/SplashActivity.java
@@ -1,7 +1,53 @@
 package de.qabel.qabelbox.activities;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+
+import de.qabel.qabelbox.R;
+
 /**
  * Created by danny on 11.01.2016.
  */
-public class SplashActivity {
+public class SplashActivity extends Activity {
+    private long SPLASH_TIME = 1000;
+    private SplashActivity mActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        setContentView(R.layout.activity_splashscreen);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            startDelayedHandler();
+        }
+    }
+
+    private void startDelayedHandler() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+
+            private void startMainActivity() {
+                Intent intent = new Intent(mActivity, MainActivity.class);
+                intent.setAction("");
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+
 }

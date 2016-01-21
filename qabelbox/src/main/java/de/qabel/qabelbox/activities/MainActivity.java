b34f4dd80de7c490abea7afe6a6c2432ffdf00a0
L-Henke <henke@praemandatum.de>
2016-01-21 16:22:04 +0100

    Added empty DropMessage handler to MainActivity

--- qabelbox/src/main/java/de/qabel/qabelbox/activities/MainActivity.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/activities/MainActivity.java
@@ -43,12 +43,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import de.qabel.core.config.Contact;
 import de.qabel.core.config.Identity;
+import de.qabel.core.drop.DropMessage;
 import de.qabel.qabelbox.QabelBoxApplication;
 import de.qabel.qabelbox.R;
 import de.qabel.qabelbox.adapter.FilesAdapter;
@@ -315,6 +317,12 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
 
+		mService.setDropMessageReceiver(new LocalQabelService.OnDropMessageReceived() {
+		@Override
+			public void onDropMessage(Collection<DropMessage> dropMessages) {
+				//TODO: Handle DropMessages
+			}
+		});
     }
 
     private void initBoxVolume(Identity activeIdentity) {

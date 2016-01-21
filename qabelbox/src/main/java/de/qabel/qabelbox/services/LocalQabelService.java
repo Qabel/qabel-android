2df0e7eda4ba5ea94ec4809a72c3c93e7c40c7e7
L-Henke <henke@praemandatum.de>
2016-01-21 15:12:58 +0100

    Extracted recieve method from DropActor

--- qabelbox/src/main/java/de/qabel/qabelbox/services/LocalQabelService.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/services/LocalQabelService.java
@@ -7,8 +7,15 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
 
+import java.net.URI;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,18 +25,25 @@ import de.qabel.core.config.Contacts;
 import de.qabel.core.config.Identities;
 import de.qabel.core.config.Identity;
 import de.qabel.core.config.Persistable;
+import de.qabel.core.crypto.AbstractBinaryDropMessage;
 import de.qabel.core.crypto.BinaryDropMessageV0;
 import de.qabel.core.crypto.CryptoUtils;
 import de.qabel.core.drop.DropMessage;
 import de.qabel.core.drop.DropURL;
+import de.qabel.core.exceptions.QblDropInvalidMessageSizeException;
 import de.qabel.core.exceptions.QblDropPayloadSizeException;
 import de.qabel.core.exceptions.QblInvalidEncryptionKeyException;
+import de.qabel.core.exceptions.QblSpoofedSenderException;
+import de.qabel.core.exceptions.QblVersionMismatchException;
 import de.qabel.core.http.DropHTTP;
 import de.qabel.core.http.HTTPResult;
 import de.qabel.qabelbox.config.AndroidPersistence;
 import de.qabel.qabelbox.config.QblSQLiteParams;
 
 public class LocalQabelService extends Service {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(LocalQabelService.class.getName());
+
 
 	private static final String TAG = "LocalQabelService";
 	private static final String PREF_LAST_ACTIVE_IDENTITY = "PREF_LAST_ACTIVE_IDENTITY";
@@ -178,6 +192,82 @@ public class LocalQabelService extends Service {
 		dropResultCallback.onSendDropResult(delivered);
 	}
 
+	/**
+	 * Retrieves all DropMessages all Identities
+	 *
+	 * @return Retrieved, decrypted DropMessages.
+	 */
+	public Collection<DropMessage> retrieveDropMessages() {
+		Collection<DropMessage> allMessages = new ArrayList<>();
+		for(Identity identity : getIdentities().getIdentities()) {
+			for(DropURL dropUrl: identity.getDropUrls()) {
+				Collection<DropMessage> results = this.retrieveDropMessages(dropUrl.getUri());
+				allMessages.addAll(results);
+			}
+		}
+		return allMessages;
+	}
+
+	/**
+	 * Retrieves all DropMessages from given URI
+	 *
+	 * @param uri      URI where to retrieve the drop from
+	 * @return Retrieved, decrypted DropMessages.
+	 */
+	public Collection<DropMessage> retrieveDropMessages(URI uri) {
+		DropHTTP http = new DropHTTP();
+		HTTPResult<Collection<byte[]>> cipherMessages = http.receiveMessages(uri);
+		Collection<DropMessage> plainMessages = new ArrayList<>();
+
+		List<Contact> ccc = new ArrayList<>(getContacts().getContacts());
+		Collections.shuffle(ccc, new SecureRandom());
+
+		for (byte[] cipherMessage : cipherMessages.getData()) {
+			AbstractBinaryDropMessage binMessage;
+			byte binaryFormatVersion = cipherMessage[0];
+
+			switch (binaryFormatVersion) {
+				case 0:
+					try {
+						binMessage = new BinaryDropMessageV0(cipherMessage);
+					} catch (QblVersionMismatchException e) {
+						LOGGER.error("Version mismatch in binary drop message", e);
+						throw new RuntimeException("Version mismatch should not happen", e);
+					} catch (QblDropInvalidMessageSizeException e) {
+						LOGGER.info("Binary drop message version 0 with unexpected size discarded.");
+						// Invalid message uploads may happen with malicious intent
+						// or by broken clients. Skip.
+						continue;
+					}
+					break;
+				default:
+					LOGGER.warn("Unknown binary drop message version " + binaryFormatVersion);
+					// cannot handle this message -> skip
+					continue;
+			}
+			for (Identity identity : getIdentities().getIdentities()) {
+				DropMessage dropMessage;
+				try {
+					dropMessage = binMessage.disassembleMessage(identity);
+				} catch (QblSpoofedSenderException e) {
+					//TODO: Notify the user about the spoofed message
+					break;
+				}
+				if (dropMessage != null) {
+					for (Contact c : ccc) {
+						if (c.getKeyIdentifier().equals(dropMessage.getSenderKeyId())){
+							if (dropMessage.registerSender(c)){
+								plainMessages.add(dropMessage);
+								break;
+							}
+						}
+					}
+					break;
+				}
+			}
+		}
+		return plainMessages;
+	}
 
 	public class LocalBinder extends Binder {
 		public LocalQabelService getService() {

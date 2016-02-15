fc759c544c5300b4ac8c70da427964a5ebb33f1a
L-Henke <henke@praemandatum.de>
2016-02-15 17:35:59 +0100

    Added external BoxObject comparator

---
+++ qabelbox/src/main/java/de/qabel/qabelbox/helper/BoxObjectComparators.java
@@ -0,0 +1,32 @@
+package de.qabel.qabelbox.helper;
+
+import java.util.Comparator;
+
+import de.qabel.qabelbox.storage.BoxFile;
+import de.qabel.qabelbox.storage.BoxFolder;
+import de.qabel.qabelbox.storage.BoxObject;
+import de.qabel.qabelbox.storage.BoxUploadingFile;
+
+public class BoxObjectComparators {
+
+	public static Comparator<BoxObject> alphabeticOrderDirectoriesFirstIgnoreCase() {
+		return new Comparator<BoxObject>() {
+			@Override
+			public int compare(BoxObject lhs, BoxObject rhs) {
+				if (lhs instanceof BoxFile && (rhs instanceof BoxFile || rhs instanceof BoxUploadingFile)) {
+					return lhs.name.compareToIgnoreCase(rhs.name);
+				}
+				if (lhs instanceof BoxUploadingFile && (rhs instanceof BoxFile || rhs instanceof BoxUploadingFile)) {
+					return lhs.name.compareToIgnoreCase(rhs.name);
+				}
+				if (lhs instanceof BoxFolder && rhs instanceof BoxFolder) {
+					return lhs.name.compareToIgnoreCase(rhs.name);
+				}
+				if (lhs instanceof BoxFolder) {
+					return -1;
+				}
+				return 1;
+			}
+		};
+	}
+}

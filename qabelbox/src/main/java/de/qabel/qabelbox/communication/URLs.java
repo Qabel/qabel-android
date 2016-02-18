d535a8b70ad2bc3ac990c470b26aa47c93523fca
Jens Kadenbach <gitdax@daxbau.net>
2016-02-18 14:34:55 +0100

    Update accounting server URL

--- qabelbox/src/main/java/de/qabel/qabelbox/communication/URLs.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/communication/URLs.java
@@ -5,9 +5,9 @@ package de.qabel.qabelbox.communication;
  */
 public class URLs {
 
-    final static String REGISTER = "https://qaccounting.prae.me/api/v0/auth/registration/";
-    final static String LOGIN = "https://qaccounting.prae.me/api/v0/auth/login/";
-    final static String LOGOUT = "https://qaccounting.prae.me/api/v0/auth/logout/";
-    final static String PASSWORD_CHANGE = "https://qaccounting.prae.me/api/v0/auth/password/change/";
-    final static String PASSWORD_RESET = "https://qaccounting.prae.me/api/v0/auth/password/reset/";
+    final static String REGISTER = "https://test-accounting.qabel.de/api/v0/auth/registration/";
+    final static String LOGIN = "https://test-accounting.qabel.de/api/v0/auth/login/";
+    final static String LOGOUT = "https://test-accounting.qabel.de/api/v0/auth/logout/";
+    final static String PASSWORD_CHANGE = "https://test-accounting.qabel.de/api/v0/auth/password/change/";
+    final static String PASSWORD_RESET = "https://test-accounting.qabel.de/api/v0/auth/password/reset/";
 }

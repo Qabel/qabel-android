80e9ac49ee37d46d7a9a7ffdd43f31948beb0d60
L-Henke <henke@praemandatum.de>
2016-02-19 17:41:14 +0100

    Adapt BoxExternalReference to use prefix and block

--- qabelbox/src/main/java/de/qabel/qabelbox/storage/BoxExternalReference.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/storage/BoxExternalReference.java
@@ -7,16 +7,22 @@ import java.util.Arrays;
 public class BoxExternalReference {
 	public boolean isFolder;
 	public String name;
-	public String url;
+	public String prefix;
+	public String block;
 	public QblECPublicKey owner;
 	public byte[] key;
 
-	public BoxExternalReference(boolean isFolder, String url, String name, QblECPublicKey owner, byte[] key) {
+	public BoxExternalReference(boolean isFolder, String prefix, String block, String name, QblECPublicKey owner, byte[] key) {
 		this.isFolder = isFolder;
 		this.name = name;
-		this.url = url;
+		this.prefix = prefix;
+		this.block = block;
 		this.owner = owner;
 		this.key = key;
+	}
+
+	public String getRef() {
+		return prefix + '/' + block;
 	}
 
 	@Override
@@ -27,7 +33,9 @@ public class BoxExternalReference {
 		BoxExternalReference that = (BoxExternalReference) o;
 
 		if (isFolder != that.isFolder) return false;
-		if (url != null ? !url.equals(that.url) : that.url != null) return false;
+		if (name != null ? !name.equals(that.name) : that.name != null) return false;
+		if (prefix != null ? !prefix.equals(that.prefix) : that.prefix != null) return false;
+		if (block != null ? !block.equals(that.block) : that.block != null) return false;
 		if (owner != null ? !owner.equals(that.owner) : that.owner != null) return false;
 		return Arrays.equals(key, that.key);
 
@@ -36,7 +44,9 @@ public class BoxExternalReference {
 	@Override
 	public int hashCode() {
 		int result = (isFolder ? 1 : 0);
-		result = 31 * result + (url != null ? url.hashCode() : 0);
+		result = 31 * result + (name != null ? name.hashCode() : 0);
+		result = 31 * result + (prefix != null ? prefix.hashCode() : 0);
+		result = 31 * result + (block != null ? block.hashCode() : 0);
 		result = 31 * result + (owner != null ? owner.hashCode() : 0);
 		result = 31 * result + (key != null ? Arrays.hashCode(key) : 0);
 		return result;
@@ -44,6 +54,6 @@ public class BoxExternalReference {
 
 	@Override
 	protected BoxExternalReference clone() throws CloneNotSupportedException {
-		return new BoxExternalReference(isFolder, url,name,owner,key);
+		return new BoxExternalReference(isFolder, prefix, block, name, owner, key);
 	}
 }

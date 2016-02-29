d9ed7b582b31cfbcecd7ddfead12e6465d9df4d2
Jan D.S. Wischweh <jw@twoboxventures.com>
2016-02-29 16:41:32 +0100

    adds Fragment stub adds licence-overview txt

---
+++ qabelbox/src/main/java/de/qabel/qabelbox/fragments/AboutLicencesFragment.java
@@ -0,0 +1,15 @@
+package de.qabel.qabelbox.fragments;
+
+/**
+ * Created by r-hold on 29.02.16.
+ */
+public class AboutLicencesFragment {
+
+    public static AboutLicencesFragment newInstance() {
+        AboutLicencesFragment fragment=new AboutLicencesFragment();
+
+        return fragment;
+    }
+
+
+}

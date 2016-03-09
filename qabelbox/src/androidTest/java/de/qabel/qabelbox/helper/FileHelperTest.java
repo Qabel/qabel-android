6df42c1f8a5cb138258881d72a6d39dfdc457f65
Jan D.S. Wischweh <jw@twoboxventures.com>
2016-03-09 14:39:23 +0100

    fixes FileHelperTest by converting it to an InstrumentationTest removes duplicate entry for testInstrumentationRunner from gradle file

--- qabelbox/src/androidTest/java/de/qabel/qabelbox/helper/FileHelperTest.java
+++ qabelbox/src/androidTest/java/de/qabel/qabelbox/helper/FileHelperTest.java
@@ -1,6 +1,7 @@
 package de.qabel.qabelbox.helper;
 
 import android.support.test.rule.ActivityTestRule;
+import android.test.InstrumentationTestCase;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,15 +10,13 @@ import de.qabel.qabelbox.activities.MainActivity;
 
 import static junit.framework.Assert.assertEquals;
 
-public class FileHelperTest {
-	@Rule
-	public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, true);
+public class FileHelperTest extends InstrumentationTestCase {
 
 	private static final String CONTENT = "This a Testfile.";
 
 	@Test
 	public void testLoadFileFromAssets(){
-		String file = FileHelper.loadFileFromAssets(mActivityTestRule.getActivity(), "html/help/test.html");
+		String file = FileHelper.loadFileFromAssets(getInstrumentation().getTargetContext(), "html/help/test.html");
 		assertEquals(CONTENT, file);
 	}
 }

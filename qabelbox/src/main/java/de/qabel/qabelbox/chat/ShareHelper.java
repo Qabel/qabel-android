e5a1aa66ae26d4dae2a6561ba18f31cb45e807d3
dan <werner@qabel.de>
2016-03-24 08:09:38 +0100

    format text

--- qabelbox/src/main/java/de/qabel/qabelbox/chat/ShareHelper.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/chat/ShareHelper.java
@@ -37,10 +37,9 @@ public class ShareHelper {
 	public static void tellAFriend(Activity activity) {
 
 		Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-		//sharingIntent.setType("text/plain");
-		sharingIntent.setType("text/html");
-		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.tellAFriendSubject);
-		sharingIntent.putExtra(Intent.EXTRA_TEXT, R.string.tellAFriendMessage);
+		sharingIntent.setType("text/plain");
+		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, activity.getString(R.string.tellAFriendSubject));
+		sharingIntent.putExtra(Intent.EXTRA_TEXT, activity.getString(R.string.tellAFriendMessage));
 		activity.startActivity(Intent.createChooser(sharingIntent, activity.getResources().getText(R.string.share_via)));
 	}
 

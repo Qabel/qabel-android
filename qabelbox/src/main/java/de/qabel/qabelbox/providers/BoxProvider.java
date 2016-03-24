020e6d3baa85f5afa445121f8eadf2995acdd601
jogi <joachimgirschik@googlemail.com>
2016-03-24 18:47:43 +0100

    changed completed icon

--- qabelbox/src/main/java/de/qabel/qabelbox/providers/BoxProvider.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/providers/BoxProvider.java
@@ -632,7 +632,7 @@ public class BoxProvider extends DocumentsProvider {
         }
         mBuilder.setContentTitle("Downloading " + filename)
                 .setContentText("Download complete")
-                .setSmallIcon(R.drawable.notification_template_icon_bg);
+                .setSmallIcon(R.drawable.qabel_logo);
         mBuilder.setProgress(100, 100, false);
         mNotifyManager.notify(id, mBuilder.build());
         File out = new File(getContext().getExternalCacheDir(), basename);

3568f6fdcebc312d5fdb97130e9aaa8393bdfdfe
Jens Kadenbach <gitdax@daxbau.net>
2016-05-04 13:24:53 +0200

    Fixed persistence migration

--- qabelbox/src/androidTest/java/de/qabel/qabelbox/persistence/MigratePersistenceTest.java
+++ qabelbox/src/androidTest/java/de/qabel/qabelbox/persistence/MigratePersistenceTest.java
@@ -5,12 +5,16 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.RenamingDelegatingContext;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import de.qabel.core.config.Contact;
 import de.qabel.core.config.Contacts;
 import de.qabel.core.config.Identity;
+import de.qabel.core.crypto.QblECKeyPair;
+import de.qabel.core.crypto.QblECPublicKey;
 import de.qabel.desktop.config.factory.DropUrlGenerator;
 import de.qabel.desktop.config.factory.IdentityBuilderFactory;
 import de.qabel.desktop.repository.IdentityRepository;
@@ -19,6 +23,7 @@ import de.qabel.desktop.repository.sqlite.SqliteContactRepository;
 import de.qabel.qabelbox.QabelBoxApplication;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -40,6 +45,7 @@ public class MigratePersistenceTest {
         Context context = new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(),
                 "test_");
         factory = new RepositoryFactory(context);
+        factory.close();
         factory.deleteDatabase();
         AndroidClientDatabase androidClientDatabase = factory.getAndroidClientDatabase();
         identityRepository = factory.getIdentityRepository(androidClientDatabase);
@@ -61,6 +67,11 @@ public class MigratePersistenceTest {
         assertThat(first.getEcPublicKey(), not(equalTo(second.getEcPublicKey())));
     }
 
+    @After
+    public void tearDown() {
+        factory.close();
+    }
+
     @Test
     public void testIdentitiesMigrated() throws Exception {
         PersistenceMigrator.migrate(androidPersistence, identityRepository, contactRepository);
@@ -69,5 +80,35 @@ public class MigratePersistenceTest {
                 notNullValue());
         assertThat("Identity 'second' not found", identityRepository.find(second.getKeyIdentifier()),
                 notNullValue());
+    }
+
+    @Test
+    public void testContactsMigrated() throws Exception {
+        Contact contact1 = new Contact("contact1", first.getDropUrls(), first.getEcPublicKey());
+        Contact contact2 = new Contact("contact2", second.getDropUrls(), second.getEcPublicKey());
+        Contact contact3 = new Contact("contact3", first.getDropUrls(), new QblECKeyPair().getPub());
+        Contacts firstContacts = new Contacts(first);
+        firstContacts.put(contact2);
+        firstContacts.put(contact1);
+        firstContacts.put(contact3);
+        androidPersistence.persistEntity(firstContacts);
+        Contacts secondContacts = new Contacts(second);
+        secondContacts.put(contact2);
+        secondContacts.put(contact1);
+        androidPersistence.persistEntity(secondContacts);
+
+        PersistenceMigrator.migrate(androidPersistence, identityRepository, contactRepository);
+        Contacts firstMigrated = contactRepository.find(first);
+        assertThat("Not all contacts found for 'first' identity",
+                firstMigrated.getContacts().size(), equalTo(3));
+        assertThat("contact1 not found in 'first'", firstMigrated.contains(contact1));
+        assertThat("contact2 not found in 'first'", firstMigrated.contains(contact2));
+        assertThat("contact3 not found in 'first'", firstMigrated.contains(contact3));
+
+        Contacts secondMigrated = contactRepository.find(second);
+        assertThat("Not all contacts found for 'second' identity",
+                secondMigrated.getContacts().size(), equalTo(2));
+        assertThat("contact1 not found in 'second'", secondMigrated.contains(contact1));
+        assertThat("contact2 not found in 'second'", secondMigrated.contains(contact2));
     }
 }

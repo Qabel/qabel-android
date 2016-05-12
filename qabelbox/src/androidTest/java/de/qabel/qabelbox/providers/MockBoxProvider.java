68de993038923973b436b454216a3eefd957be27
Jens Kadenbach <gitdax@daxbau.net>
2016-05-12 13:50:03 +0200

    Random prefix in provider test

--- qabelbox/src/androidTest/java/de/qabel/qabelbox/providers/MockBoxProvider.java
+++ qabelbox/src/androidTest/java/de/qabel/qabelbox/providers/MockBoxProvider.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import org.spongycastle.util.encoders.Hex;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import de.qabel.core.config.Identities;
 import de.qabel.core.config.Identity;
@@ -25,7 +26,7 @@ public class MockBoxProvider extends BoxProvider {
     public String lastID;
     public QblECKeyPair keyPair;
     public String rootDocId;
-    public final String prefix = "test";
+    public String prefix;
     public static final String PUB_KEY = "8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a";
     public static final String PRIVATE_KEY = "77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a";
     public Identity identity;
@@ -52,6 +53,7 @@ public class MockBoxProvider extends BoxProvider {
     }
 
     private void setParametersForTests() {
+        prefix = UUID.randomUUID().toString();
         CryptoUtils utils = new CryptoUtils();
         deviceID = utils.getRandomBytes(16);
         keyPair = new QblECKeyPair(Hex.decode(PRIVATE_KEY));

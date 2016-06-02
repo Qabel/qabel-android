06e983f3fd9f19e0b2e4ac03fffd6c22a98f0811
JoGir <girschik@qabel.de>
2016-06-02 20:31:06 +0200

    completed storagenotification tests

--- qabelbox/src/main/java/de/qabel/qabelbox/storage/model/BoxUploadingFile.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/storage/model/BoxUploadingFile.java
@@ -4,8 +4,8 @@ public class BoxUploadingFile extends BoxObject {
 
     private String path;
     private String ownerIdentifier;
-    public long totalSize;
-    public long uploadedSize;
+    public long totalSize = 1;
+    public long uploadedSize = 0;
 
     public BoxUploadingFile(String name, String path, String ownerIdentifier) {
         super(name);

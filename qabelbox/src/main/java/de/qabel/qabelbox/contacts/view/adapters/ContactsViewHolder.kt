47b352cbcdeea2e4100e99ea4bf7be61c2bef262
JoGir <girschik@qabel.de>
2016-06-20 01:43:41 +0200

    improve layout

--- qabelbox/src/main/java/de/qabel/qabelbox/contacts/view/adapters/ContactsViewHolder.kt
+++ qabelbox/src/main/java/de/qabel/qabelbox/contacts/view/adapters/ContactsViewHolder.kt
@@ -28,6 +28,8 @@ class ContactsViewHolder(itemView: View, val clickListener: (ContactDto) -> Unit
         itemView?.alpha = if (contact.identities.size > 0) 1f else 0.35f
 
         itemView?.textViewItemName?.text = contact.contact.alias
+        //TODO Field for Mail/Phone
+        //itemView?.textViewItemDetail?.text = contact.contact.email
         itemView?.tv_initial?.text = getInitials(contact.contact.alias);
         itemView?.contact_icon_border?.background = ContactIconDrawable(colors)
         itemView?.setOnClickListener({ clickListener.invoke(contact) });

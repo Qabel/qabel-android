b47d026c1f8100bf86780a52d64c21b0f280eb69
JoGir <girschik@qabel.de>
2016-07-11 17:54:07 +0200

    add simple uitest for about view

---
+++ qabelbox/src/androidTest/java/de/qabel/qabelbox/ui/AboutUITest.kt
@@ -0,0 +1,27 @@
+package de.qabel.qabelbox.ui
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.*
+import android.support.test.espresso.contrib.DrawerActions
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.RootMatchers.*
+import de.qabel.qabelbox.R
+import de.qabel.qabelbox.ui.matcher.ToolbarMatcher
+import org.junit.Test
+
+class AboutUITest : AbstractUITest(){
+
+    @Test
+    fun testShowAbout() {
+        launchActivity(null)
+        DrawerActions.openDrawer(R.id.drawer_layout)
+        onView(withText(R.string.action_about)).perform(click())
+
+        ToolbarMatcher.matchToolbarTitle(mActivity.getString(R.string.action_about))
+        onView(withId(R.id.about_licences_list)).check(matches(isDisplayed()))
+
+        onView(withText(R.string.about_header_qabel_show_btn)).perform(click());
+        onView(withText(R.string.qapl)).inRoot(isDialog()).check(matches(isDisplayed()));
+    }
+}

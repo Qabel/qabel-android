cc4c35ab6b41fa4ebad546edc98abad3b9b22b29
Jens Kadenbach <gitdax@daxbau.net>
2016-07-26 13:19:11 +0200

    Update for changes in core

--- qabelbox/src/androidTest/java/de/qabel/qabelbox/box/BoxVolumeImplTest.kt
+++ qabelbox/src/androidTest/java/de/qabel/qabelbox/box/BoxVolumeImplTest.kt
@@ -1,10 +1,11 @@
 package de.qabel.qabelbox.box
 
 import de.qabel.box.storage.AbstractMetadata
-import de.qabel.box.storage.AndroidBoxVolume
 import de.qabel.box.storage.BoxVolumeConfig
+import de.qabel.box.storage.BoxVolumeImpl
 import de.qabel.box.storage.jdbc.DirectoryMetadataDatabase
-import de.qabel.box.storage.tryWith
+import de.qabel.box.storage.jdbc.JdbcDirectoryMetadataFactory
+import de.qabel.core.repositories.AndroidVersionAdapter
 import de.qabel.qabelbox.box.backends.MockStorageBackend
 import de.qabel.qabelbox.util.IdentityHelper
 import org.junit.Test
@@ -17,13 +18,19 @@ class BoxVolumeImplTest {
     @Test
     fun testNavigate() {
         val backend = MockStorageBackend()
-        val volume = AndroidBoxVolume(BoxVolumeConfig(
+        val volume = BoxVolumeImpl(BoxVolumeConfig(
                 "prefix",
                 byteArrayOf(1,2,3,4),
                 backend,
                 backend,
                 "Blacke2b",
-                createTempDir()), identity.primaryKeyPair)
+                createTempDir(),
+                directoryMetadataFactoryFactory = {
+                    JdbcDirectoryMetadataFactory(it.tempDir, it.deviceId) { connection ->
+                        DirectoryMetadataDatabase(connection, AndroidVersionAdapter(connection))
+                    }
+                }),
+                identity.primaryKeyPair )
         volume.createIndex("qabel", "prefix")
         volume.navigate()
     }
@@ -33,11 +40,10 @@ class BoxVolumeImplTest {
         val connection = DriverManager.getConnection(
                 AbstractMetadata.JDBC_PREFIX + createTempFile().absolutePath)
         connection.autoCommit = true
-        tryWith(connection.createStatement()) {execute("PRAGMA journal_mode=MEMORY") }
-        val db = DirectoryMetadataDatabase(connection)
+        val db = DirectoryMetadataDatabase(connection,
+                versionAdapter = AndroidVersionAdapter(connection))
         db.migrate()
         db.migrate()
     }
-
 }
 

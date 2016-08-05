199c8015af6702859968aae5ece5814c1abcdfeb
Jens Kadenbach <gitdax@daxbau.net>
2016-08-05 17:01:54 +0200

    Manual sync every 3 hours instead of 5 minutes

--- qabelbox/src/main/java/de/qabel/qabelbox/helper/AccountHelper.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/helper/AccountHelper.java
@@ -18,8 +18,8 @@ public class AccountHelper {
     private static final String ACCOUNT_TYPE = BuildConfig.ACCOUNT_TYPE;
     public static final Account DEFAULT_ACCOUNT = new Account(ACCOUNT, ACCOUNT_TYPE);
     public static final String AUTHORITY = BuildConfig.AUTHORITY;
-    // every 5 minutes
-    public static long SYNC_INTERVAL = 60*5;
+    // every 3 hours
+    public static long SYNC_INTERVAL = 60*60*3;
     private static final String TAG = "AccountHelper";
 
     public static void createSyncAccount(Context context) {

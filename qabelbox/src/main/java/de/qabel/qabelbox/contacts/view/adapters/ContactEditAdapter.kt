a20498ed558e98ab469b02e6f59c8b1eca2efa6d
JoGir <girschik@qabel.de>
2016-08-16 20:28:25 +0200

    use identity icon in contactedit view

--- qabelbox/src/main/java/de/qabel/qabelbox/contacts/view/adapters/ContactEditAdapter.kt
+++ qabelbox/src/main/java/de/qabel/qabelbox/contacts/view/adapters/ContactEditAdapter.kt
@@ -9,6 +9,7 @@ import de.qabel.qabelbox.R
 import de.qabel.qabelbox.contacts.dto.ContactDto
 import de.qabel.qabelbox.contacts.extensions.*
 import de.qabel.qabelbox.contacts.view.widgets.ContactIconDrawable
+import de.qabel.qabelbox.contacts.view.widgets.IdentityIconDrawable
 import kotlinx.android.synthetic.main.contact_edit_identity_switch.view.*
 import kotlinx.android.synthetic.main.fragment_contact_edit.view.*
 import org.jetbrains.anko.layoutInflater
@@ -39,7 +40,9 @@ class ContactEditAdapter() {
                     it.isChecked = active
                 }
                 it.setTag(R.id.identity_switch_control, identity.id)
-                it.identity_icon.background = ContactIconDrawable(listOf(identity.color(context)))
+                it.identity_icon.background = IdentityIconDrawable(
+                        text = identity.initials(),
+                        color = identity.color(context))
                 it.identity_initial.text = identity.initials()
                 contact_identities.addView(it)
             }

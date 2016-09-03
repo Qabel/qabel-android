ee1814ddee181191bb91cc3379b4e71e4f4a0645
JoGir <girschik@qabel.de>
2016-09-03 21:10:44 +0200

    update wizards

---
+++ qabelbox/src/main/java/de/qabel/qabelbox/helper/formatters.kt
@@ -0,0 +1,12 @@
+package de.qabel.qabelbox.helper
+
+import com.google.i18n.phonenumbers.NumberParseException
+import com.google.i18n.phonenumbers.PhoneNumberUtil
+import java.util.*
+
+@Throws(NumberParseException::class)
+fun formatPhoneNumber(phone: String): String {
+    val phoneUtil = PhoneNumberUtil.getInstance()
+    val parsedPhone = phoneUtil.parse(phone, Locale.getDefault().country)
+    return PhoneNumberUtil.getInstance().format(parsedPhone, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL)
+}

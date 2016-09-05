6ed97ae65f764efdabba573e874fcb76bee4103a
JoGi <girschik@qabel.de>
2016-09-05 21:43:53 +0200

    models

--- qabelbox/src/main/java/de/qabel/qabelbox/index/models.kt
+++ qabelbox/src/main/java/de/qabel/qabelbox/index/models.kt
@@ -1,5 +1,6 @@
 package de.qabel.qabelbox.index
 
+import de.qabel.core.index.FieldType
 
 //TODO coreable
 data class RawContact(val displayName: String,
@@ -7,3 +8,16 @@ data class RawContact(val displayName: String,
                       val emailAddresses: List<String>,
                       val identifier: String? = null //external identifier
 )
+
+enum class EntryStatus {
+    MODIFIED,
+    PENDING,
+    VERIFIED
+}
+
+// Directly add verified flags to identity
+//TODO OR
+data class IdentityIndexStatus(val identityId: Int, val emailStatus: EntryStatus, val phoneStatus: EntryStatus)
+
+// TODO OR
+data class IndexEntry(val value: String, val type: FieldType, val status: EntryStatus)

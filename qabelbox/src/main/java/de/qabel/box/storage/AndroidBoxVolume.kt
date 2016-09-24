b525507ec7cb48581d7349c2700b095b16cfe636
JoGi <girschik@qabel.de>
2016-09-24 13:47:32 +0200

    integrate core changes

--- qabelbox/src/main/java/de/qabel/box/storage/AndroidBoxVolume.kt
+++ qabelbox/src/main/java/de/qabel/box/storage/AndroidBoxVolume.kt
@@ -1,8 +1,16 @@
 package de.qabel.box.storage
 
+import de.qabel.box.storage.jdbc.JdbcFileMetadataFactory
 import de.qabel.core.crypto.QblECKeyPair
+import de.qabel.core.repositories.AndroidVersionAdapter
 
 class AndroidBoxVolume(config: BoxVolumeConfig, keyPair: QblECKeyPair) : BoxVolumeImpl(config, keyPair) {
+
+    companion object {
+        init {
+            JdbcFileMetadataFactory.versionAdapterFactory = ::AndroidVersionAdapter
+        }
+    }
 
     override fun loadDriver() {
         Class.forName("org.sqldroid.SQLDroidDriver")

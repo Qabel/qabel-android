7c876903871c76664ae7cbdd54cc6900ee5fa296
JoGi <girschik@qabel.de>
2016-09-25 23:17:05 +0200

    change edit avatar

--- qabelbox/src/main/java/de/qabel/qabelbox/contacts/view/adapters/ContactEditAdapter.kt
+++ qabelbox/src/main/java/de/qabel/qabelbox/contacts/view/adapters/ContactEditAdapter.kt
@@ -9,11 +9,13 @@ import de.qabel.core.ui.displayName
 import de.qabel.core.ui.initials
 import de.qabel.qabelbox.R
 import de.qabel.qabelbox.contacts.dto.ContactDto
-import de.qabel.qabelbox.contacts.extensions.*
+import de.qabel.qabelbox.contacts.extensions.color
+import de.qabel.qabelbox.contacts.extensions.contactColors
 import de.qabel.qabelbox.contacts.view.widgets.ContactIconDrawable
 import de.qabel.qabelbox.contacts.view.widgets.IdentityIconDrawable
 import kotlinx.android.synthetic.main.contact_edit_identity_switch.view.*
 import kotlinx.android.synthetic.main.fragment_contact_edit.view.*
+import org.jetbrains.anko.dip
 import org.jetbrains.anko.layoutInflater
 
 
@@ -24,7 +26,11 @@ class ContactEditAdapter() {
     fun loadContact(contact: ContactDto, identities: Identities) {
         view?.apply {
             contact_icon_border.background = ContactIconDrawable(contact.contactColors(context))
-            tv_initial.text = contact.contact.initials()
+            val size = dip(95)
+            contact_initials.background = IdentityIconDrawable(text = contact.contact.initials(),
+                    width = size, height = size,
+                    color = contact.contact.color(context))
+
             editTextContactNick.setText(contact.contact.displayName())
             editTextContactName.text = contact.contact.alias
             contact_ignored_switch.isChecked = contact.contact.isIgnored

17be398d23bf8ea06b3177018db9e6319f3968f2
Jens Kadenbach <kadenbach@qabel.de>
2016-11-04 11:20:54 +0100

    Remove legacy test until the core-test-utils package lives

--- qabelbox/src/androidTest/java/de/qabel/qabelbox/config/IdentityExportImportTest.java
+++
@@ -1,8 +0,0 @@
-package de.qabel.qabelbox.config;
-
-/**
- * Legacy test for IdentityImportExport. Critical lib org.json
- */
-public class IdentityExportImportTest extends de.qabel.core.config.IdentityExportImportTest {
-
-}

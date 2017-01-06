cf19c606d8935f283bfaf815efa80bdbbd837806
Jens Kadenbach <kadenbach@qabel.de>
2017-01-06 17:07:31 +0100

    Fixes crash when AndroidChatServiceResponder is somehow called with a null intent
    Resolves #816

--- qabelbox/src/main/java/de/qabel/qabelbox/chat/services/AndroidChatServiceResponder.kt
+++ qabelbox/src/main/java/de/qabel/qabelbox/chat/services/AndroidChatServiceResponder.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import de.qabel.qabelbox.QblBroadcastConstants
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.warn
 
 /**
  * Starts the Service on receiving QblBroadcastConstants.Chat.NOTIFY_NEW_MESSAGES
@@ -15,14 +16,20 @@ class AndroidChatServiceResponder : BroadcastReceiver(), AnkoLogger {
 
     override fun onReceive(context: Context, intent: Intent?) {
         info("Receive notify broadcast. Start service")
+        if (intent == null) {
+            warn("AndroidChatServiceResponder called with a null intent")
+            return
+        }
         var action = QblBroadcastConstants.Chat.Service.MESSAGES_READ
-        if (intent?.action == QblBroadcastConstants.Chat.NOTIFY_NEW_MESSAGES) {
+        if (intent.action == QblBroadcastConstants.Chat.NOTIFY_NEW_MESSAGES) {
             action = QblBroadcastConstants.Chat.Service.NOTIFY
             abortBroadcast()
         }
         context.applicationContext.startService(Intent(action,
                 null, context.applicationContext, AndroidChatService::class.java).apply {
-            putExtras(intent?.extras)
+            intent.extras.let {
+                putExtras(it)
+            }
         })
     }
 
